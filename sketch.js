3faa576bc2363c54a89eb44967b76bb6190bb6ce
Michelle <michelle.tlee888@gmail.com>
2024-12-05 23:23:20 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -15,10 +15,32 @@ function draw() {
 
 function main() {
     background("white");
+    textFont('Courier New');
+    fill("#6F4F37");
+    
+    textAlign(CENTER)
+
+    push()
+    textStyle(BOLDITALIC);
+    let fontSize = windowWidth/50; // You can change this ratio to suit your needs
+    textSize(fontSize);
+    text("hit play to start!", windowWidth / 2, windowHeight*(2/3));
+    pop()
+
+    push();
+    fontSize = windowWidth/80; // You can change this ratio to suit your needs
+    textSize(fontSize);
+    text("© codingmiche", windowWidth/2, windowHeight*(3/4))
+    pop();
+
 }
 
 function board() {
-    background("pink");
+    background("white");
+    text("board", windowWidth / 2, windowHeight / 2)
+
+    stroke(30);
+    rect(windowWidth / 16, windowHeight / 8, windowWidth * (2 / 3), windowHeight / 2);
 }
 
 function windowResized() {
