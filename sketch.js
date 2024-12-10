daddb783c404cbd86632fce868c32244947e9a64
Michelle <michelle.tlee888@gmail.com>
2024-12-10 02:26:12 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let currentPage = 0; // page variable
 let playButton;
+let myLogo;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -13,11 +14,34 @@ function draw() {
     }
 }
 
+function preload() {
+    myLogo = loadImage("mleepaints.jpeg")
+}
+
 function main() {
     background("white");
     textFont('Courier New');
     fill("#6F4F37");
     textAlign(CENTER);
+
+    // var scale = 0.8;
+    // imageMode(CENTER);
+    // image(img, 0.5*width, 0.5*height, scale*width, scale*img.height*width/img.width); // to fit width
+
+    push();
+    imageMode(CENTER);
+  
+    // dynamically calculate dimensions based on window size
+    let scaleFactorWidth = windowWidth * 0.9 / myLogo.width;
+    let scaleFactorHeight = windowHeight * 0.7 / myLogo.height;
+  
+    // choose the smaller scaling factor to maintain aspect ratio and fit within the window
+    let scaleFactor = Math.min(scaleFactorWidth, scaleFactorHeight);
+  
+    // Draw the image dynamically resized
+    image(myLogo, windowWidth/2, windowHeight*(0.5)-125, myLogo.width * scaleFactor, myLogo.height * scaleFactor);
+    pop();
+
 
     push();
     textStyle(BOLDITALIC);
@@ -28,9 +52,9 @@ function main() {
 
     // the play button only exists on the main page
 
-    const buttonWidth = windowWidth * 0.1;
-    const buttonHeight = windowHeight * (0.03);
-    const buttonFontSize = windowWidth / 120;
+    let buttonWidth = windowWidth * 0.1;
+    let buttonHeight = windowHeight * (0.03);
+    let buttonFontSize = windowWidth / 120;
 
     // reference for `${}` : https://stackoverflow.com/questions/35835362/what-does-dollar-sign-and-curly-braces-mean-in-a-string-in-javascript
     // reference for css: https://www.w3schools.com/cssref/index.php
@@ -40,7 +64,7 @@ function main() {
         playButton.style("font-family", "Courier New");
         playButton.style("border-radius", '40px')
         playButton.style("font-size", `${buttonFontSize}px`);
-        playButton.style("background-color", "#fec5d4"); 
+        playButton.style("background-color", "#fec5d4");
         playButton.style("color", "#6F4F37")
         playButton.style("border-color", "#6F4F37")
         playButton.style("width", `${buttonWidth}px`);
